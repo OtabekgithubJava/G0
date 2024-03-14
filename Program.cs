81f829733e91d48d7aa5718492065deefa67a43a
Mr. Otabek <146438558+OtabekgithubJava@users.noreply.github.com>
2024-03-14 17:12:30 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace DTM
     {
         static async Task Main(string[] args)
         {
-            const string token = "6077885052:AAFJays-peGn1YKsD8W9vkORfLN569YsiO4";
+            const string token = "6077885052:AAFJays-peGn1YKsD8W9vkORfLN569YsiO4"; // Gotta delete it
             
             // TelegramBotHandler.TestNumber = 1; 
             // TelegramBotHandler handler = new TelegramBotHandler(token);
